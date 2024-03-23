a943d31dc26dffd05a47758fee51eeda0c681bf4
Md Ashfaque <kashfaque557@gmail.com>
2024-03-24 02:05:48 +0530

    order route added

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -1,11 +1,15 @@
 import express from 'express'
 
 import {adminOnly}  from "../middlewares/auth.middleware.js"
+import { newOrder } from '../controllers/order.controller.js';
 
 const router = express.Router();
 
 
 
+
+
+router.route("/new-order").post(newOrder)
 
 
 
