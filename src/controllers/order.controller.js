cebf047818ed8e416052076f145c1d4c89c89a2c
Md Ashfaque <kashfaque557@gmail.com>
2024-03-24 02:08:16 +0530

    order controller added

---
+++ src/controllers/order.controller.js
@@ -0,0 +1,21 @@
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+
+
+
+const newOrder = asyncHandler(async(req,res) => {
+
+})
+
+
+
+
+
+
+
+
+
+export {
+    newOrder,
+}
